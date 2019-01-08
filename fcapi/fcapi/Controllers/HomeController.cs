792c2e2f1f5a8a88469e4b5ed9c70d4fbfc19b3c
Sreejith Vijayan <Sreejithvij@gmail.com>
2019-01-08 09:47:01 +0530

    Added config values

--- fcapi/fcapi/Controllers/HomeController.cs
+++ fcapi/fcapi/Controllers/HomeController.cs
@@ -10,9 +10,10 @@ namespace fcapi.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Title = "Home Page";
+            //ViewBag.Title = "Home Page";
 
-            return View();
+            //return View();
+            return new RedirectResult("~/swagger");
         }
     }
 }
